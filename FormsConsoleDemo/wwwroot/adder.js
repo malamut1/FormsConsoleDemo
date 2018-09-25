302c80e6d6dcd2c4ff01a79d11d8bd3987aeccc6
olanmj <olanm@stockton.edu>
2018-09-25 09:32:32 -0400

    Add project files.

---
+++ FormsConsoleDemo/wwwroot/adder.js
@@ -0,0 +1,7 @@
+﻿let button = document.querySelector("#add");
+button.onclick = function () {
+    let answer = Number(document.querySelector("#op1").value) +
+        Number(document.querySelector("#op2").value);
+    document.querySelector("#answer").textContent = answer;
+    return false;
+}
